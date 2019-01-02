ae5df7f3e5e025d30773a4cf46a0a8af66a8a390
高天 <tian.gao@qunar.com>
2019-01-03 00:06:59 +0800

    update

--- src/problems/tinytest/xmltest/XmlReader.java
+++ src/problems/tinytest/xmltest/XmlReader.java
@@ -1,14 +1,10 @@
 package problems.tinytest.xmltest;
 
-import com.alibaba.fastjson.JSONObject;
-import net.sf.json.xml.XMLSerializer;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.Node;
-import org.dom4j.io.SAXReader;
-import java.io.File;
-import java.util.Iterator;
+import org.json.JSONObject;
+import org.json.XML;
+
+import java.io.*;
+import java.util.Map;
 
 /**
  * @author tian.gao
@@ -16,35 +12,24 @@ import java.util.Iterator;
  **/
 public class XmlReader {
 
-    public static JSONObject parseXml(String filename) {
+    public static Map<String, Object> parseXml(String filename) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(new File(filename)));
+            BufferedReader br = new BufferedReader(reader);
+            String readLine;
+            while ((readLine = br.readLine()) != null) {
+                sb.append(readLine);
+            }
+        } catch (IOException e) {
+            System.out.println("read file error");
+        }
+        JSONObject jsonObject = XML.toJSONObject(sb.toString());
+        return jsonObject.toMap();
     }
 
     public static void main(String[] args) {
-        SAXReader reader = new SAXReader();
-        try {
-            final Document doc = reader.read(new File("/Users/sortinn/Desktop/test.xml"));
-            Iterator<Node> iterator = doc.nodeIterator();
-            while (iterator.hasNext()) {
-                final Node node = iterator.next();
-                final String nodeName = node.getName();
-                System.out.println(nodeName);
-
-                if (node instanceof Element) {
-                    final Element element = (Element) node;
-                    final Iterator<Node> elementIterator = element.nodeIterator();
-                    while (elementIterator.hasNext()) {
-                        final Node elementNext = elementIterator.next();
-                        final String elementNextName = elementNext.getName();
-                        if (elementNextName == null) {
-                            continue;
-                        }
-                        System.out.println(elementNextName);
-                    }
-                }
-            }
-
-        } catch (DocumentException e) {
-            System.out.println("read file error.");
-        }
+        final Map<String, Object> stringObjectMap = parseXml("/Users/sortinn/Desktop/test.xml");
+        System.out.println(stringObjectMap);
     }
 }
